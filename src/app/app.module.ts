25c21d7b87e62c9b0246e5c7fbfbb10344528693
mayur rohokale <mayurrohokale12345@gmail.com>
2023-06-11 19:48:31 +0530

    First Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { Demo2Component } from './demo2/demo2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent,
+    Demo2Component
   ],
   imports: [
     BrowserModule
